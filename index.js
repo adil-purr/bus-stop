ee69dba9ed8c7508d600c905692da5361d8af1d7
Adil Ali <adilali@purrdigital.com>
2017-02-03 17:17:45 +0000

    blah blah

--- index.js
+++ index.js
@@ -22,36 +22,84 @@ http.listen(3000, function(){
 app.use(express.static('public'))
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+  socket.on('busCreate', function(msg){
+  bus = new Bus(msg.number, msg.routeName, msg.time)
+  allBuses.push(bus)
   });
 
   socket.on('bus message', function(msg){
-    console.log(msg)
+    // console.log(msg)
     allBuses.push(msg)
   })
 
 
+
+
   setInterval(function(){
+    // console.log(allBuses)
     io.emit('allBusTimes', allBuses)
-  }, 2000)
+    io.emit('queTime', queTime)
+  }, 500)
+
+
 
 });
 
 var allBuses = []
+var queTime = {}
+
+function Bus(number, routeName, time, id){
+  this.number = number;
+  this.routeName = routeName;
+  this.time = time || 100;
+  this.id = id || allBuses.length + 1
+}
+
+bus = new Bus(1, 'Trafalgar Square')
+allBuses.push(bus)
+bus2 = new Bus(2, 'Blackfriars', 150, 2)
+allBuses.push(bus2)
+
 
 setInterval(function(){
   for (var i = 0; i < allBuses.length; i++) {
-    allBuses[i].time -= 1
-    if(allBuses[i].time <= 0){
+    busTick(allBuses[i])
+    if(allBuses[i].time < 0){
       allBuses.splice(i, 1)
     }
-
   }
 }, 1000)
+
+setInterval(function(){
+  bus = new Bus(1, 'Trafalgar Square')
+  allBuses.push(bus)
+}, 100000)
+
+setInterval(function(){
+  bus = new Bus(2, 'Black Friars')
+  allBuses.push(bus)
+}, 80000)
+
+setInterval(function(){
+  timeArray = allBuses.map(function(a){ return a.time;})
+  totalSeconds = timeArray.reduce(function(a,b){return a + b}, 0)
+  queTime['minutes'] = Math.floor(totalSeconds / 60);
+  queTime['seconds'] = totalSeconds - queTime['minutes'] * 60;
+  console.log(queTime)
+}, 500)
+
+
 
 
 
 var busTick = function(bus){
   bus.time -= 1
+}
+
+var calculateQueTime = function(){
+  timeArray = allBuses.map(function(a){ return a.time;})
+  totalSeconds = timeArray.reduce(function(a,b){return a + b}, 0)
+  queTime['minutes'] = Math.floor(totalSeconds / 60);
+  queTime['seconds'] = totalSeconds - queTime['minutes'] * 60;
+  console.log(queTime)
 }
--- public/main.js
+++ public/main.js
@@ -1,40 +1,76 @@
+testBus = {
+ 'number': '1',
+ 'time': 10
+}
+
+
 $(document).ready(function(){
-  var socket = io();
+var socket = io.connect('http://localhost:3000');
 
 
-  $('#send').on('click',function(){
-    number = $('#m').val();
-    bus = {
-      'number': number,
-      'time' : 100
-    }
-    socket.emit('bus message', bus);
-    $('#m').val('');
-    return false;
+
+  $('#book').on('click',function(){
+    var bus = {}
+    bus['number']= $('#number').val();
+    bus['routeName']= $('#routeName').val();
+    bus['time']= $('#time').val();
+    console.log(bus)
+    socket.emit('busCreate', bus)
+    $('.container input').val('')
   });
 
-  $('#getTimes').on('click', function(){
-    socket.emit('getTimes');
-  })
 
   socket.on('allBusTimes', function(buses){
-    // console.log(buses)
+    $('#totalBuses').text('Total Buses: ' + buses.length)
     updateBusElements(buses)
   })
 
-})
+  socket.on('queTime', function(queTime){
+    $('#que').text('Time till last bus ' + str_pad_left(queTime.minutes, '0', 2) + ':' + str_pad_left(queTime.seconds, '0', 2))
+  })
 
-var createBusElement = function(bus){
-  $('.time-box').prepend('<div class="bus number' + bus.number +'">Bus Number:'+bus.number+' is coming in <p class="time">' +bus.time +'</p> seconds.</div>')
-}
 
-var updateBusElements = function(busArray){
-  for (var i = 0; i < busArray.length; i++) {
-    if($('.bus.number'+busArray[i].number).length == 1){
-      $('.bus.number'+busArray[i].number).children().first().text(busArray[i].time);
-    }
-    else{
-      createBusElement(busArray[i])
+  var createBusElement = function(bus){
+    $('.time-box').prepend('<div id="bus'+bus.id+'" class="bus number' + bus.number +'">Bus Number: '+bus.number+' to '+ bus.routeName +' is coming in <p class="time">' +bus.time +'</p> seconds.</div>')
+  }
+
+  var updateBusElements = function(busArray){
+    for (var i = 0; i < busArray.length; i++) {
+      if( existingBus(busArray[i]) ){
+        updateBus(busArray[i])
+      }
+      else{
+        createBusElement(busArray[i])
+      }
     }
   }
+
+
+  var updateBus = function(bus){
+    if(bus.time <= 0){
+      departingBus(bus)
+    }
+    else{
+      $('#bus'+bus.id).children().first().text(bus.time);
+    }
+
+  }
+
+  var existingBus = function(bus){
+    return $('#bus'+bus.id).length === 1
+  }
+
+  var departingBus = function(bus){
+    $('.bus.number'+bus.number).text('Bus number '+ bus.number +' has arrived')
+
+    setTimeout(function(){
+      $('#bus'+bus.id).remove()
+    }, 500)
+
+  }
+
+})
+
+function str_pad_left(string,pad,length) {
+    return (new Array(length+1).join(pad)+string).slice(-length);
 }
